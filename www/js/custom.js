71a7952bd16c61b6f1098631757b023b36c21ed6
AKCL <brad.hawkins1@aucklandcouncil.govt.nz>
2014-06-04 16:45:21 +1200

    Updated JSON

--- www/js/custom.js
+++ www/js/custom.js
@@ -3,6 +3,17 @@
 $(document).ready(function(){
 
   $('.bg-image').backstretch("img/BritishArtists.Nash2.jpg");
-  $('.copy').backstretch("img/old.paper.jpg");
+  $('.copy').backstretch("img/old.pxaper.jpg");
+
+
+alert("test");
+  // timeline code
+
+  // process JSON
+
+  $.getJSON("timeline/events.js", function( json ) {
+  console.log(json);
+ });
+
 
 })
