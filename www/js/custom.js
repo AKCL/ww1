f30f6fb21ab7921b3c7d631d8bcf1b54be527ae5
AKCL <brad.hawkins1@aucklandcouncil.govt.nz>
2014-06-17 17:50:28 +1200

    Ajax progressing - back button working now

--- www/js/custom.js
+++ www/js/custom.js
@@ -7,158 +7,189 @@ $('.bg-image').backstretch("img/BritishArtists.Nash2.jpg");
 $('.copy').backstretch("img/old.pxaper.jpg");
 
 
-	function ig(){
+function ig(){
 
-			// We only want these styles applied when javascript is enabled
-			$('div.content').css('display', 'block');
+	// We only want these styles applied when javascript is enabled
+	$('div.content').css('display', 'block');
 
-			// Initially set opacity on thumbs and add
-			// additional styling for hover effect on thumbs
-			var onMouseOutOpacity = 0.5;
+	// Initially set opacity on thumbs and add
+	// additional styling for hover effect on thumbs
+	var onMouseOutOpacity = 0.5;
 
-			$('#thumbs ul.thumbs li, div.navigation a.pageLink').opacityrollover({
-			    mouseOutOpacity:   onMouseOutOpacity,
-			    mouseOverOpacity:  1.0,
-			    fadeSpeed:         'fast',
-			    exemptionSelector: '.selected'
-			});
-
-
-			// Initialize Advanced Galleriffic Gallery
-			var gallery = $('#thumbs').galleriffic({
-			    delay:                     2500,
-			    numThumbs:                 15,
-			    preloadAhead:              10,
-			    enableTopPager:             1,
-			    enableBottomPager:         1,
-			    imageContainerSel:         '#slideshow',
-			    controlsContainerSel:      '#controls',
-			    captionContainerSel:       '#caption',
-			    loadingContainerSel:       '#loading',
-			    renderSSControls:          0,
-			    renderNavControls:         true,
-			    playLinkText:              'Play Slideshow',
-			    pauseLinkText:             'Pause Slideshow',
-			    prevLinkText:              '&lsaquo; Previous',
-			    nextLinkText:              'Next &rsaquo;',
-			    nextPageLinkText:          'Next &rsaquo;',
-			    prevPageLinkText:          '&lsaquo; Prev',
-			    enableHistory:             false,
-			    autoStart:                 false,
-			    syncTransitions:           true,
-			    defaultTransitionDuration: 900,
-			    onSlideChange:             function(prevIndex, nextIndex) {
-			        // 'this' refers to the gallery, which is an extension of $('#thumbs')
-			        this.find('ul.thumbs').children()
-			            .eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
-			            .eq(nextIndex).fadeTo('fast', 1.0);
-
-			        // Update the photo index display
-			        this.$captionContainer.find('div.photo-index')
-			            .html('Photo '+ (nextIndex+1) +' of '+ this.data.length);
-			    },
-			    onPageTransitionOut:       function(callback) {
-			        this.fadeTo('fast', 0.0, callback);
-			    },
-			    onPageTransitionIn:        function() {
-			        var prevPageLink = this.find('a.prev').css('visibility', 'hidden');
-			        var nextPageLink = this.find('a.next').css('visibility', 'hidden');
-			        
-			        // Show appropriate next / prev page links
-			        if (this.displayedPage > 0)
-			            prevPageLink.css('visibility', 'visible');
-
-			        var lastPage = this.getNumPages() - 1;
-			        if (this.displayedPage < lastPage)
-			            nextPageLink.css('visibility', 'visible');
-
-			        this.fadeTo('fast', 1.0);
-			    }
-			});
+	$('#thumbs ul.thumbs li, div.navigation a.pageLink').opacityrollover({
+	    mouseOutOpacity:   onMouseOutOpacity,
+	    mouseOverOpacity:  1.0,
+	    fadeSpeed:         'fast',
+	    exemptionSelector: '.selected'
+	});
 
 
+	// Initialize Advanced Galleriffic Gallery
+	var gallery = $('#thumbs').galleriffic({
+	    delay:                     2500,
+	    numThumbs:                 15,
+	    preloadAhead:              10,
+	    enableTopPager:             1,
+	    enableBottomPager:         1,
+	    imageContainerSel:         '#slideshow',
+	    controlsContainerSel:      '#controls',
+	    captionContainerSel:       '#caption',
+	    loadingContainerSel:       '#loading',
+	    renderSSControls:          0,
+	    renderNavControls:         true,
+	    playLinkText:              'Play Slideshow',
+	    pauseLinkText:             'Pause Slideshow',
+	    prevLinkText:              '&lsaquo; Previous',
+	    nextLinkText:              'Next &rsaquo;',
+	    nextPageLinkText:          'Next &rsaquo;',
+	    prevPageLinkText:          '&lsaquo; Prev',
+	    enableHistory:             false,
+	    autoStart:                 false,
+	    syncTransitions:           true,
+	    defaultTransitionDuration: 900,
+	    onSlideChange:             function(prevIndex, nextIndex) {
+	        // 'this' refers to the gallery, which is an extension of $('#thumbs')
+	        this.find('ul.thumbs').children()
+	            .eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
+	            .eq(nextIndex).fadeTo('fast', 1.0);
+
+	        // Update the photo index display
+	        this.$captionContainer.find('div.photo-index')
+	            .html('Photo '+ (nextIndex+1) +' of '+ this.data.length);
+	    },
+	    onPageTransitionOut:       function(callback) {
+	        this.fadeTo('fast', 0.0, callback);
+	    },
+	    onPageTransitionIn:        function() {
+	        var prevPageLink = this.find('a.prev').css('visibility', 'hidden');
+	        var nextPageLink = this.find('a.next').css('visibility', 'hidden');
+	        
+	        // Show appropriate next / prev page links
+	        if (this.displayedPage > 0)
+	            prevPageLink.css('visibility', 'visible');
+
+	        var lastPage = this.getNumPages() - 1;
+	        if (this.displayedPage < lastPage)
+	            nextPageLink.css('visibility', 'visible');
+
+	        this.fadeTo('fast', 1.0);
+	    }
+	});
 
 
-			/**************** Event handlers for custom next / prev page links **********************/
-
-			gallery.find('a.prev').click(function(e) {
-			    gallery.previousPage();
-			    e.preventDefault();
-			});
-
-			gallery.find('a.next').click(function(e) {
-			    gallery.nextPage();
-			    e.preventDefault();
-			});
-
-			/****************************************************************************************/
-
-			// /**** Functions to support integration of galleriffic with the jquery.history plugin ****/
-
-			// // PageLoad function
-			// // This function is called when:
-			// // 1. after calling $.historyInit();
-			// // 2. after calling $.historyLoad();
-			// // 3. after pushing "Go Back" button of a browser
-			// function pageload(hash) {
-			//     // alert("pageload: " + hash);
-			//     // hash doesn't contain the first # character.
-			//     if(hash) {
-			//         $.galleriffic.gotoImage(hash);
-			//     } else {
-			//         gallery.gotoIndex(0);
-			//     }
-			// }
-
-			// // Initialize history plugin.
-			// // The callback is called at once by present location.hash. 
-			// $.historyInit(pageload);
-
-			// // set onlick event for buttons using the jQuery 1.3 live method
-			// $("a[rel='history']").live('click', function(e) {
-			//     if (e.button != 0) return true;
-
-			//     var hash = this.href;
-			//     hash = hash.replace(/^.*#/, '');
-
-			//     // moves to a new page. 
-			//     // pageload is called at once. 
-			//     // hash don't contain "#", "?"
-			//     $.historyLoad(hash);
-
-			//     return false;
-			// });
 
 
-			} // end of ig()
+	/**************** Event handlers for custom next / prev page links **********************/
+
+	gallery.find('a.prev').click(function(e) {
+	    gallery.previousPage();
+	    e.preventDefault();
+	});
+
+	gallery.find('a.next').click(function(e) {
+	    gallery.nextPage();
+	    e.preventDefault();
+	});
+
+	/****************************************************************************************/
+
+	// /**** Functions to support integration of galleriffic with the jquery.history plugin ****/
+
+	// // PageLoad function
+	// // This function is called when:
+	// // 1. after calling $.historyInit();
+	// // 2. after calling $.historyLoad();
+	// // 3. after pushing "Go Back" button of a browser
+	// function pageload(hash) {
+	//     // alert("pageload: " + hash);
+	//     // hash doesn't contain the first # character.
+	//     if(hash) {
+	//         $.galleriffic.gotoImage(hash);
+	//     } else {
+	//         gallery.gotoIndex(0);
+	//     }
+	// }
+
+	// // Initialize history plugin.
+	// // The callback is called at once by present location.hash. 
+	// $.historyInit(pageload);
+
+	// // set onlick event for buttons using the jQuery 1.3 live method
+	// $("a[rel='history']").live('click', function(e) {
+	//     if (e.button != 0) return true;
+
+	//     var hash = this.href;
+	//     hash = hash.replace(/^.*#/, '');
+
+	//     // moves to a new page. 
+	//     // pageload is called at once. 
+	//     // hash don't contain "#", "?"
+	//     $.historyLoad(hash);
+
+	//     return false;
+	// });
+
+
+	} // end of ig()
+
+	// http://rosspenman.com/pushstate-jquery/ more ...
+	
+
+	var popped = ('state' in window.history && window.history.state !== null),
+	initialUrl = location.pathname,
+	$ajaxDest = $('.thumbs'),
+	$galleryHeading = $('.gallery h2');
+
+
 	$(document).on("click", ".case-nav a", function(e) {	
 
-		var target = $(this).data("url"),
-		title = $(this).html();
-		$thumbs = $('.thumbs');
-
-		history.pushState({}, '', $(this).attr("href"));
-
 		
-		$thumbs.load(target, function(){
-	  		ig();
-	  		//$.historyInit(pageload);
-	  		ajaxLoaded(title);
-		});
-
-		 $(window).on("popstate", function() {
-		 	alert("back");
-    		loadPage(location.target);
-
-  		});
-
-		function ajaxLoaded(title){
-			$('.gallery h2').html(title);
-
+		var title = $(this).html(),
+		ajaxSrc = $(this).attr("href"),
+		$caseNav = $('.case-nav a');
+		
+		if(history.pushState) {
+			history.pushState({'title':title}, title, ajaxSrc);
 		}
 
+	
 		e.preventDefault();
 
-	});
+
+
+		getContent(ajaxSrc, title);
+
+
+		$caseNav.removeClass('selected');
+		$(this).parent().addClass('selected');
+
+		//console.log(initialURL);
+
+	}); //  end case-nav ajax
+
+
+	
+	function getContent(ajaxSrc, title){
+		$ajaxDest.load(ajaxSrc, function(){
+		  	ig();
+		  	$galleryHeading.html(title);
+		});
+	}
+
+	window.onpopstate = function(event) {
+		//alert("load the previous gallery");
+
+		console.log("pathname: "+location.pathname);
+		console.log(initialUrl);
+		
+		if (location.pathname != initialUrl){
+			getContent(location.pathname, "???? title"); /// get the title?? Could retrive from the ajaxed file ...
+		}
+		
+	};
+
+
+	
+
     /****************************************************************************************/
 });
